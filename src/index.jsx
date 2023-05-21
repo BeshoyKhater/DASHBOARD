d36c0124ed48b589703424dab43107ef1f6a5276
BeshoyKhater <BeshoyKhater@gmail.com>
2023-05-21 15:33:04 +0300

    demo

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,15 @@ import * as ReactDOMClient from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/scss/main.scss';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
